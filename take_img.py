ca074e6b46021a71bda948a1a2a7ff363dc792aa
mazidulhasanpip <93870967+mazidulhasanpip@users.noreply.github.com>
2021-11-09 23:30:38 +0600

    Updated the firebase credentials

--- take_img.py
+++ take_img.py
@@ -3,17 +3,18 @@ import datetime
 import time
 import pyrebase
 from zipfile import ZipFile
-
+import psutil
+    
 #firebase Credentials
 firebaseConfig = {
-    'apiKey': "AIzaSyAPDMY23mkmDib0G_NB7WovgKqceByWmSg",
-    'authDomain': "raspimage-2b328.firebaseapp.com",
-    'databaseURL': "https://raspimage-2b328-default-rtdb.firebaseio.com",
-    'projectId': "raspimage-2b328",
-    'storageBucket': "raspimage-2b328.appspot.com",
-    'messagingSenderId': "915090438516",
-    'appId': "1:915090438516:web:3bac95b92c463feed95421",
-    'measurementId': "G-6RE6EMQFSY"  
+  'apiKey': "AIzaSyAeVxMEE7BM5tdELx9jsQwNDe4bVjCDTl0",
+  'authDomain': "emeter-image-transmission.firebaseapp.com",
+  'databaseURL': "https://emeter-image-transmission-default-rtdb.firebaseio.com",
+  'projectId': "emeter-image-transmission",
+  'storageBucket': "emeter-image-transmission.appspot.com",
+  'messagingSenderId': "516882488455",
+  'appId': "1:516882488455:web:f410318990794dc6a2ac22",
+  'measurementId': "G-VXPMTX2QRP"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig) 
@@ -37,7 +38,7 @@ os.system('/home/pi/Desktop/piCodes/check_all_files.sh')
 
 #give a name with write mode on
 currentdate_hour = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")
-zipObj = ZipFile('/home/pi/Desktop/piCodes/zip_files/'+currentdate_hour, 'w')
+zipObj = ZipFile('/home/pi/Desktop/piCodes/zip_files/'+currentdate_hour+".zip", 'w')
 
 #this will take image according to the given range
 for x in range(2):
@@ -58,6 +59,7 @@ for x in range(2):
 # close the Zip File
 zipObj.close()
 
-#send zipped file to FRDC folder of firebase 
-storage.child('FRDC/'+currentdate_hour).put('/home/pi/Desktop/piCodes/zip_files/'+currentdate_hour)
+#send zipped file to FRDC folder of firebase
+print("Uploading file...")
+storage.child('FRDC/'+currentdate_hour+".zip").put('/home/pi/Desktop/piCodes/zip_files/'+currentdate_hour+".zip")
 print("Zip upload done")
